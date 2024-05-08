e3c3d1f9cb7314d18a015cb73468b575edb43516
Abdulmalik <ayodejiamzat@gmail.com>
2024-05-08 21:45:36 +0100

    my pull

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Button = (props) => {
     const {nameOfButton} = props
   return (
+    <>
+    <div>ye github</div>
     <button>{nameOfButton}</button>
+    </>
   )
 }
 
